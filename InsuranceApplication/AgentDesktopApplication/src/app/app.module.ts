87353cb38693c66fd00150dfb9de868b1ad02704
narsinghraom <narsinghraom.dhatriinfo@gmail.com>
2019-10-03 00:56:18 +0530

    project initiated- phase2

--- InsuranceApplication/AgentDesktopApplication/src/app/app.module.ts
+++ InsuranceApplication/AgentDesktopApplication/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkspaceComponent
   ],
   imports: [
     BrowserModule,
